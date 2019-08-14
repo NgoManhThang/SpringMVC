af305ff15554b6f467269e2e3d9a4bdae8fc9500
NgoManhThang <manhthangngo1994@gmail.com>
2019-08-14 16:21:15 +0700

    initial

---
+++ src/main/java/thangnm/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package thangnm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/")
+    public String init(){
+        return "init";
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/main/java/thangnm/webconfig/SpringWebConfig.java
@@ -0,0 +1,22 @@
+package thangnm.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"thangnm"})
+public class SpringWebConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
